99faac905d68e8a9f3f6b7313434689f6f847bf1
UmakatsuYusuke <s1971021@s.konan-u.ac.jp>
2020-11-13 16:27:02 +0900

    コミット

--- app/src/main/java/com/example/endlessrun/ResultActivity.java
+++ app/src/main/java/com/example/endlessrun/ResultActivity.java
@@ -2,7 +2,10 @@ package com.example.endlessrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Button restartButton =findViewById(R.id.RestartButton);
+        restartButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent =new Intent(ResultActivity.this,StartActivity.class);
+                    startActivity(intent);
+            }
+        });
+
+
     }
 }

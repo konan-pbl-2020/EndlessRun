a7d43e5d9cdea9d6e525532f24dd03ed456ee12f
s1971026 <takewhitesword1615@gmail.com>
2020-11-13 16:26:47 +0900

    asaassa

--- app/src/main/java/com/example/endlessrun/StartActivity.java
+++ app/src/main/java/com/example/endlessrun/StartActivity.java
@@ -2,7 +2,10 @@ package com.example.endlessrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button nextButton = (Button)findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
